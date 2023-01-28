fdcf18bdb7d8401c8db55ed40726e3e0d4fe1071
Huichoman <rockbotica@gmail.com>
2023-01-27 23:00:49 -0600

    Fix fetchData.js

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,9 +1,6 @@
 export const fetchData = async (keyWord) => {
   console.log("keyWord > ", keyWord);
   const endPoint = `https://pokeapi.co/api/v2/pokemon/${keyWord}`;
-  const apiKey = "rnY4RhqwuO7FUe1Fl9Q93dzyTKOqZ337";
-  //   const endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyWord}&limit=5&offset=0&rating=g&lang=en`;
-
   const response = await fetch(endPoint);
   const data = await response.json();
   const pokemonData = {
@@ -13,7 +10,8 @@ export const fetchData = async (keyWord) => {
     image: data.sprites.other.dream_world.front_default,
   };
 
-  console.log(pokemonData);
+  return pokemonData;
+  // console.log(pokemonData);
   //   console.log("response >", response);
   console.log("data >", data);
 };
